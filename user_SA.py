fd24180eec398dd1f8e594b6e4e9d4c77e48a78b
Caitlin Sanders <cmsander@syr.edu>
2019-04-16 16:56:42 -0400

    Sentiment analysis scores implemented

--- user_SA.py
+++ user_SA.py
@@ -1,23 +1,43 @@
+from textblob import TextBlob
 from header import *
 from harvest import *
 
 twitter_api = oauth_login()
-screen_name = 'caitsands'
+
+# Add Twitter user you want tweets from here
+screen_name = ''
 user_tweets = harvest_user_timeline(twitter_api, screen_name)
 
-cat_names = ["cat", "kitten", "kitty"]
-dog_names = ["dog", "doggy", "pupper", "doggo", "puppy"]
+# Classifying terms. Going to add more.
+cat_names = [" cat ", " kitten ", " kitty ", " meow ", " feline "]
+dog_names = [" dog ", " doggy ", " pupper ", " doggo ", \
+             " puppy ", " woof ", " borker ", " yapper ", \
+             " hound ", " golden retriever ", " siberian husky ",]
 
+# Lists of sentiment scores per each tweet
 final_cat_scores = []
 final_dog_scores = []
 
 for tweet in user_tweets:
-     if any(name in tweet.lower() for name in cat_names):
-        # get sentiment analsyis for that tweet and add to final_cat_scores
-         final_cat_scores.append(1)
-     if any(name in tweet.lower() for name in dog_names):
-        # get sentiment analsyis for that tweet and add to final_dog_scores
-        final_dog_scores.append(1)
+    if any(name in tweet.lower() for name in cat_names) and \
+       any(name in tweet.lower() for name in dog_names):
+        # Need to add way to parse sentance if contains both cat
+        # and dog classifiers (i.e. "I hate cats but I love dogs")
+        # should add a negative sentiment score to final_cat_scores
+        # and positive to final_dog_scores
+        break
+    elif any(name in tweet.lower() for name in cat_names):
+         # print("Cat: ",tweet)
+         blob = TextBlob(tweet)
+         final_cat_scores.append(blob.sentiment.polarity)
+    elif any(name in tweet.lower() for name in dog_names):
+        # print("Dog: ",tweet)
+        blob = TextBlob(tweet)
+        final_dog_scores.append(blob.sentiment.polarity)
 
-print(''.join(str(e) for e in final_cat_scores))
-print(''.join(str(e) for e in final_dog_scores))
+# Sum of sentiment scores for all tweets containing cat or dog classifiers
+cat_sa_score = sum(final_cat_scores)
+dog_sa_score = sum(final_dog_scores)
+
+print("Final dog score: ", dog_sa_score)
+print("Final cat score: ", cat_sa_score)
