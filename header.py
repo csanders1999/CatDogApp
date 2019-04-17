f05d7fc64ca762e89ce6d263070cb6d7ab80a105
Tyson Owens <tysoneowens@gmail.com>
2019-04-17 15:23:27 -0400

    research for movies demo for cat and dog

--- header.py
+++ header.py
@@ -11,6 +11,7 @@ from sys import maxsize as maxint
 from credentials import *
 from ibm_watson import PersonalityInsightsV3
 from os.path import join, dirname
+from textblob import TextBlob
 import time
 import os
 
@@ -197,3 +198,50 @@ def harvest_user_timeline(twitter_api, screen_name=None, user_id=None, max_resul
     print('Done fetching tweets', file=sys.stderr)
 
     return results[:max_results]
+
+def get_friends_followers_ids(twitter_api, screen_name=None, user_id=None,
+                              friends_limit=maxint, followers_limit=maxint):
+
+    # Must have either screen_name or user_id (logical xor)
+    assert (screen_name != None) != (user_id != None),     "Must have screen_name or user_id, but not both"
+
+    # See http://bit.ly/2GcjKJP and http://bit.ly/2rFz90N for details
+    # on API parameters
+
+    get_friends_ids = partial(make_twitter_request, twitter_api.friends.ids,
+                              count=5000)
+    get_followers_ids = partial(make_twitter_request, twitter_api.followers.ids,
+                                count=5000)
+
+    friends_ids, followers_ids = [], []
+
+    for twitter_api_func, limit, ids, label in [
+                    [get_friends_ids, friends_limit, friends_ids, "friends"],
+                    [get_followers_ids, followers_limit, followers_ids, "followers"]
+                ]:
+
+        if limit == 0: continue
+
+        cursor = -1
+        while cursor != 0:
+
+            # Use make_twitter_request via the partially bound callable...
+            if screen_name:
+                response = twitter_api_func(screen_name=screen_name, cursor=cursor)
+            else: # user_id
+                response = twitter_api_func(user_id=user_id, cursor=cursor)
+
+            if response is not None:
+                ids += response['ids']
+                cursor = response['next_cursor']
+
+            #print('Fetched {0} total {1} ids for {2}'.format(len(ids),                  label, (user_id or screen_name)),file=sys.stderr)
+
+            # XXX: You may want to store data during each iteration to provide an
+            # an additional layer of protection from exceptional circumstances
+
+            if len(ids) >= limit or response is None:
+                break
+
+    # Do something useful with the IDs, like store them to disk...
+    return friends_ids[:friends_limit], followers_ids[:followers_limit]
--- personalityAnalysisDemo.py
+++ personalityAnalysisDemo.py
@@ -19,7 +19,7 @@ def ibm_login():
 ###############################################################################
 ###############################################################################
 ##                                                                           ##
-##                         Where the Magic Happens                           ##
+##                         My Functions and Methods                          ##
 ##                                                                           ##
 ###############################################################################
 ###############################################################################
@@ -34,13 +34,23 @@ def reduce_status(s):
         'language': s['lang']
     }
 
+###############################################################################
+###############################################################################
+##                                                                           ##
+##                         WHERE THE MAGIC HAPPENS                           ##
+##                                                                           ##
+###############################################################################
+###############################################################################
+
 if __name__ == "__main__":
     twitter_api = oauth_login()
     personality_insights = ibm_login()
 
-    user_list = ['tysonowens','caitsands','Joe_Comeau3',]
+    user_list = ['TysonOwens']
+    dog_list = []
+    cat_list = []
 
-    research = {
+    research_dog = {
         'movie_romance' : 0,
         'movie_adventure' : 0,
         'movie_horror' : 0,
@@ -53,9 +63,75 @@ if __name__ == "__main__":
         'movie_document' : 0
     }
 
+    research_cat = research_dog.copy()
+
+    # Classifying terms. Going to add more.
+    cat_names = [" cat ", " kitten ", " kitty ", " meow ", " feline "]
+    dog_names = [" dog ", " doggy ", " pupper ", " doggo ", \
+                 " puppy ", " woof ", " borker ", " yapper ", \
+                 " hound ", " golden retriever ", " siberian husky ",]
+
+    MAXUSER = 1000
+
+    ############### CLASSIIFY USERS AS CAT OR DOG PERSON #####################
+    max_users = MAXUSER
     for user in user_list:
+        if max_users <= 0:
+            break
+        print("Fetching more people...")
+        if len(user_list) < MAXUSER:
+            friends_ids, followers_ids = get_friends_followers_ids(twitter_api,
+                                                                   screen_name=user,
+                                                                   friends_limit=50,
+                                                                   followers_limit=50)
+            friends_ids = get_user_profile(twitter_api, user_ids=list(friends_ids))
+            for userID in friends_ids:
+                if friends_ids[userID]['screen_name'] not in user_list:
+                    user_list.append(friends_ids[userID]['screen_name'])
+
+        print('Attempting to classify ', user)
+        user_tweets = harvest_user_timeline(twitter_api, screen_name=user, max_results=200)
+
+        # Lists of sentiment scores per each tweet
+        final_cat_scores = []
+        final_dog_scores = []
+        for tweet in user_tweets:
+            tweet = tweet['text']
+            if any(name in tweet.lower() for name in cat_names) and \
+               any(name in tweet.lower() for name in dog_names):
+                # Need to add way to parse sentance if contains both cat
+                # and dog classifiers (i.e. "I hate cats but I love dogs")
+                # should add a negative sentiment score to final_cat_scores
+                # and positive to final_dog_scores
+                break
+            elif any(name in tweet.lower() for name in cat_names):
+                 # print("Cat: ",tweet)
+                 blob = TextBlob(tweet)
+                 final_cat_scores.append(blob.sentiment.polarity)
+            elif any(name in tweet.lower() for name in dog_names):
+                # print("Dog: ",tweet)
+                blob = TextBlob(tweet)
+                final_dog_scores.append(blob.sentiment.polarity)
+
+        # Sum of sentiment scores for all tweets containing cat or dog classifiers
+        cat_sa_score = sum(final_cat_scores)
+        dog_sa_score = sum(final_dog_scores)
+
+        if dog_sa_score > cat_sa_score:
+            print(user, " is a dog person. (Dog: ", dog_sa_score, ", Cat: ", cat_sa_score, ")")
+            dog_list.append(user)
+        elif cat_sa_score > dog_sa_score:
+            print(user, " is a cat person. (Dog: ", dog_sa_score, ", Cat: ", cat_sa_score, ")")
+            cat_list.append(user)
+        else:
+            print(user, " could not be classified. (Dog: ", dog_sa_score, ", Cat: ", cat_sa_score, ")")
+        max_users = max_users-1
+
+    ################## TAKE AVERAGE SCORES FOR ALL DOG PEOPLE #################
+
+    for user in dog_list:
         newTweets = []
-        print('Fetching tweets of ', user)
+        print('Fetching tweets of dog user: ', user)
         tweets = harvest_user_timeline(twitter_api, screen_name=user, max_results=200)
         for i in tweets:
             newTweets.append(reduce_status(i))
@@ -73,20 +149,61 @@ if __name__ == "__main__":
                 raw_scores=True
             ).get_result()
 
-        research['movie_romance'] += profile['consumption_preferences'][4]['consumption_preferences'][0]['score']
-        research['movie_adventure'] += profile['consumption_preferences'][4]['consumption_preferences'][1]['score']
-        research['movie_horror'] += profile['consumption_preferences'][4]['consumption_preferences'][2]['score']
-        research['movie_musical'] += profile['consumption_preferences'][4]['consumption_preferences'][3]['score']
-        research['movie_historical'] += profile['consumption_preferences'][4]['consumption_preferences'][4]['score']
-        research['movie_scifi'] += profile['consumption_preferences'][4]['consumption_preferences'][5]['score']
-        research['movie_war'] += profile['consumption_preferences'][4]['consumption_preferences'][6]['score']
-        research['movie_drama'] += profile['consumption_preferences'][4]['consumption_preferences'][7]['score']
-        research['movie_action'] += profile['consumption_preferences'][4]['consumption_preferences'][8]['score']
-        research['movie_document'] += profile['consumption_preferences'][4]['consumption_preferences'][9]['score']
+        research_dog['movie_romance'] += profile['consumption_preferences'][4]['consumption_preferences'][0]['score']
+        research_dog['movie_adventure'] += profile['consumption_preferences'][4]['consumption_preferences'][1]['score']
+        research_dog['movie_horror'] += profile['consumption_preferences'][4]['consumption_preferences'][2]['score']
+        research_dog['movie_musical'] += profile['consumption_preferences'][4]['consumption_preferences'][3]['score']
+        research_dog['movie_historical'] += profile['consumption_preferences'][4]['consumption_preferences'][4]['score']
+        research_dog['movie_scifi'] += profile['consumption_preferences'][4]['consumption_preferences'][5]['score']
+        research_dog['movie_war'] += profile['consumption_preferences'][4]['consumption_preferences'][6]['score']
+        research_dog['movie_drama'] += profile['consumption_preferences'][4]['consumption_preferences'][7]['score']
+        research_dog['movie_action'] += profile['consumption_preferences'][4]['consumption_preferences'][8]['score']
+        research_dog['movie_document'] += profile['consumption_preferences'][4]['consumption_preferences'][9]['score']
 
-    for r in research:
-        research[r] = research[r]/len(user_list)
-    print('\nAverages found:')
-    for r in research:
-        print(r, ': ', research[r])
+
+    ################## TAKE AVERAGE SCORES FOR ALL CAT PEOPLE #################
+
+    for user in cat_list:
+        newTweets = []
+        print('Fetching tweets of cat user: ', user)
+        tweets = harvest_user_timeline(twitter_api, screen_name=user, max_results=200)
+        for i in tweets:
+            newTweets.append(reduce_status(i))
+        tweets= {'contentItems': newTweets}
+
+        with open('./profile.json', 'w') as fp:
+            json.dump(tweets, fp, indent=2)
+
+        with open(join(dirname(__file__), './profile.json')) as profile_json:
+            profile = personality_insights.profile(
+                profile_json.read(),
+                'application/json',
+                content_type='application/json',
+                consumption_preferences=True,
+                raw_scores=True
+            ).get_result()
+
+        research_cat['movie_romance'] += profile['consumption_preferences'][4]['consumption_preferences'][0]['score']
+        research_cat['movie_adventure'] += profile['consumption_preferences'][4]['consumption_preferences'][1]['score']
+        research_cat['movie_horror'] += profile['consumption_preferences'][4]['consumption_preferences'][2]['score']
+        research_cat['movie_musical'] += profile['consumption_preferences'][4]['consumption_preferences'][3]['score']
+        research_cat['movie_historical'] += profile['consumption_preferences'][4]['consumption_preferences'][4]['score']
+        research_cat['movie_scifi'] += profile['consumption_preferences'][4]['consumption_preferences'][5]['score']
+        research_cat['movie_war'] += profile['consumption_preferences'][4]['consumption_preferences'][6]['score']
+        research_cat['movie_drama'] += profile['consumption_preferences'][4]['consumption_preferences'][7]['score']
+        research_cat['movie_action'] += profile['consumption_preferences'][4]['consumption_preferences'][8]['score']
+        research_cat['movie_document'] += profile['consumption_preferences'][4]['consumption_preferences'][9]['score']
+
+    ############# PRINT AVERAGES FOR BOTH DOG AND CAT PEOPLE ##################
+
+    for r in research_dog:
+        research_dog[r] = research_dog[r]/len(dog_list)
+    for r in research_cat:
+        research_cat[r] = research_cat[r]/len(cat_list)
+    print('\nAverages found for dog users:')
+    for r in research_dog:
+        print(r, ': ', research_dog[r])
+    print('\nAverages found for cat users:')
+    for r in research_cat:
+        print(r, ': ', research_cat[r])
     os.remove('./profile.json')
