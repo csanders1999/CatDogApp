3a65f88cac2e3949137d1a6e89495057f4dbf68f
Caitlin Sanders <cmsander@syr.edu>
2019-04-24 19:09:49 -0400

    Adding comments

--- image_rec.py
+++ image_rec.py
@@ -1,20 +1,22 @@
-import json
-import re
+from header import *
+visual_recognition = VisualRecognitionV3(myVersion, iam_apikey=myIam_apikey)
 
-from watson_developer_cloud import VisualRecognitionV3
+myTweet = 'This is Beaux. He would like to box, but only if you go easy on him. Might hit you with a left hook or maybe a swift puppercut. 12/10 https://pbs.twimg.com/media/D4ZMVZSUYAAH6nv.jpg'
 
-visual_recognition = VisualRecognitionV3('2018-03-19', iam_apikey='MmKAR85BG57mM7tR5pw5T2g_U0z3t0uZLNECjnKACjN-')
-
-myString = 'This is Beaux. He would like to box, but only if you go easy on him. Might hit you with a left hook or maybe a swift puppercut. 12/10 https://pbs.twimg.com/media/D4ZMVZSUYAAH6nv.jpg'
 dog_images = 0
 cat_images = 0
-url_search = re.search("(?P<url>https?://[^\s]+)", myString)
-if url_search is not None:
-    url = url_search.group("url")
-    classes_result = visual_recognition.classify(url=url).get_result()
-    data = json.dumps(classes_result["images"][0]["classifiers"][0]["classes"], indent=2)
-    list = json.loads(data)
-    for dict in list:
+
+url_search = re.search("(?P<url>https?://[^\s]+)", myTweet) # Looks for image url in tweet
+
+if url_search is not None: # If no image, doesn't do anything
+    url = url_search.group("url") # grabs URL
+    classes_result = visual_recognition.classify(url=url).get_result() # classifies image
+
+    # Gets json data
+    classify_data = json.dumps(classes_result["images"][0]["classifiers"][0]["classes"], indent=2)
+
+    # Going through every dictionary in list of json date
+    for dict in json.loads(classify_data):
         for key, value in dict.items():
             if value == 'dog':
                 dog_images += 1
@@ -22,5 +24,6 @@ if url_search is not None:
             if value == 'cat':
                 cat_images += 1
                 break
+
 print(dog_images)
 print(cat_images)
